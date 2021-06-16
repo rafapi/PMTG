20e239325c620bc912ebc4dfd1a2f50238e5895f
rafapi <rafa@elementai.com>
2021-06-16 13:16:54 +0000

    Fix some name inconsistencies

--- tg_env.py
+++ tg_env.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 
 class BasicTG:
     def __init__(self):
-        self.numPts = 200
-        self.totalTime = 1.0
+        self.num_pts = 200
+        self.total_time = 1.0
 
-        self.currentIndex = 0
+        self.current_index = 0
 
-        self.x = np.zeros(self.numPts)
-        self.y = np.zeros(self.numPts)
+        self.x = np.zeros(self.num_pts)
+        self.y = np.zeros(self.num_pts)
 
         # Parameterize the eight shape path
         self.a_x_start = 2.0
@@ -19,11 +19,11 @@ class BasicTG:
         self.a_x = self.a_x_start
         self.a_y = self.a_y_start
 
-        self.a_x_history = np.zeros(self.numPts)
-        self.a_y_history = np.zeros(self.numPts)
+        self.a_x_history = np.zeros(self.num_pts)
+        self.a_y_history = np.zeros(self.num_pts)
 
-        for i in range(self.numPts):
-            nextTime = (float(i)/self.numPts) * self.totalTime
+        for i in range(self.num_pts):
+            nextTime = (float(i)/self.num_pts) * self.total_time
             next_x = self.a_x * np.sin(2 * np.pi * nextTime)
             next_y = (self.a_y/2.0) * (np.sin(2 * np.pi * nextTime) * np.cos(2 * np.pi * nextTime))
 
@@ -36,48 +36,49 @@ class BasicTG:
             self.a_y_history[i] = self.a_y
 
     def compute_tg_at_index(self, time, a_x, a_y):
-        time = (float(time)/self.numPts) * self.totalTime
+        time = (float(time)/self.num_pts) * self.total_time
         next_x = a_x * np.sin(2 * np.pi * time)
         next_y = (a_y/2.0) * (np.sin(2 * np.pi * time) * np.cos(2 * np.pi * time))
 
         return next_x, next_y
 
-    """Change the parameters that determine the path so that the
-    optimal path is not a perfect figure 8"""
     def degrade_path(self):
-        self.a_x = self.a_x - (2 * self.a_x /self.numPts)/10.0
-        self.a_y = self.a_y - (self.a_y / (2 * self.numPts))/10.0
+        '''
+            Add some deformation to the infinity fig
+        '''
+        self.a_x = self.a_x - (2 * self.a_x / self.num_pts)/10.0
+        self.a_y = self.a_y - (self.a_y / (2 * self.num_pts))/10.0
 
     def view_plot(self):
         plt.scatter(self.x[0:50], self.y[0:50])
         plt.show()
 
     def reset(self):
-        self.currentIndex = 0
+        self.current_index = 0
 
         self.a_x = self.a_x_start
         self.a_y = self.a_y_start
 
-        return self.currentIndex, self.x[self.currentIndex], self.y[self.currentIndex], self.a_x, self.a_y
+        return (self.current_index, self.x[self.current_index],
+                self.y[self.current_index], self.a_x, self.a_y)
 
     def reward(self, x, y):
-        optimal_x, optimal_y = self.x[self.currentIndex], self.y[self.currentIndex]
+        optimal_x, optimal_y = (self.x[self.current_index],
+                                self.y[self.current_index])
 
         return -np.sqrt(((x - optimal_x)**2) + ((y - optimal_y)**2))
 
     def step(self, new_a_x, new_a_y, nn_x, nn_y):
-
-        #self.currentIndex = self.currentIndex + 1
-
         # need to recompute the TG's at index value
-        x_tg, y_tg = self.compute_tg_at_index(self.currentIndex, new_a_x, new_a_y)
+        x_tg, y_tg = self.compute_tg_at_index(self.current_index,
+                                              new_a_x, new_a_y)
 
-        self.currentIndex = self.currentIndex + 1
+        self.current_index = self.current_index + 1
         # [time, agent's_x, agent's_y, TG's a_x, TG's a_y]
-        return self.currentIndex, (x_tg + nn_x), (y_tg + nn_y), new_a_x, new_a_y
+        return (self.current_index, (x_tg + nn_x), (y_tg + nn_y),
+                new_a_x, new_a_y)
 
     def is_done(self):
-        #if (self.currentIndex >= self.numPts - 2):
-        if (self.currentIndex >= self.numPts - 1):
+        if (self.current_index >= self.num_pts - 1):
             return True
         return False
